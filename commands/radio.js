d80c0acfc60a582fcf98dfee0a009df347017c7e
David Almeyda <dav.almeyda@gmail.com>
2022-01-22 23:48:22 -0500

    abc

---
+++ commands/radio.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { createReadStream } = require("fs");
+const { createAudioResource, StreamType, createAudioPlayer, joinVoiceChannel } = require("@discordjs/voice");
+
+module.exports = {
+  data: new SlashCommandBuilder().setName("radio").setDescription("Iniciar Radio Music Okey"),
+  // .addStringOption((option) => option.setName("song").setDescription("musica a tocar").setRequired(true))
+  async execute(interaction) {
+    const { voice } = interaction.member;
+    if (!voice.channelId) {
+      return interaction.reply("You must be in a voice channel");
+    }
+    const connection = joinVoiceChannel({
+      channelId: interaction.member.voice.channel.id,
+      guildId: interaction.guild.id,
+      adapterCreator: interaction.guild.voiceAdapterCreator,
+    });
+
+    const player = createAudioPlayer();
+
+    const resource = createAudioResource("http://play.musicokey.com:8048/stream");
+
+    resource.volume = 1;
+
+    connection.subscribe(player);
+
+    player.play(resource);
+
+    await interaction.reply("Radio");
+  },
+};
---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const { Client, Collection, Intents } = require("discord.js");
+const { token } = require("./config.json");
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+const eventFiles = fs.readdirSync("./events").filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(client, ...args));
+  }
+}
+
+client.login(token);
